49fcc09edbe341e3fce5c34408325808b5401734
Eduardo-JReis <edu.publicidade81@gmail.com>
2021-07-18 21:25:24 -0300

    Footer updated

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,9 +8,9 @@ export const Footer = () => {
             <div id="contact">
                 <p>Desenvolvido por Eduardo Reis</p>
                 <div className="icons">
-                    <a href="https://github.com/Eduardo-JReis" target="_blank" ><FaGithub color="#969cb3" /></a>
+                    <a href="https://github.com/Eduardo-JReis"><FaGithub color="#969cb3" /></a>
                     
-                    <a href="https://www.linkedin.com/in/eduardo-reis-73a16512b/" target="_blank"><FaLinkedin color="#969cb3"/></a>
+                    <a href="https://www.linkedin.com/in/eduardo-reis-73a16512b/"><FaLinkedin color="#969cb3"/></a>
                 </div>
             </div>
         </FooterContent>
